7f5f7c8a9cdfcee9716d79a7746d80b76fc9a526
Arimoroolamilekan <arimoroolamilekan1@gmail.com>
2021-03-26 22:55:28 +0100

    committed

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,15 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import TaskListItem from "./TaskListItem";
 import { useSelector } from "react-redux";
 
 const TaskList = () => {
-  const tasks = useSelector((state) => state);
+  const tasks = useSelector((tasks) => tasks);
+  const [filterTasks, setfilterTasks] = useState("");
+  const [filterResult, setfilterResult] = useState([]);
+
+  useEffect(() => {
+    const myFilterResult = tasks.filter((filteredTask) =>
+      filteredTask.taskName.toLowerCase().includes(filterTasks)
+    );
+    setfilterResult(myFilterResult);
+  }, [tasks, filterTasks]);
 
   return (
     <div className="task-list">
       <h4 className="py-3">Tasks</h4>
-      {tasks.map((task) => {
-        return <TaskListItem key={task.id} task={task} />;
+      <div>
+        <span>
+          <input
+            type="text"
+            className="filter"
+            value={filterTasks}
+            onChange={(e) => setfilterTasks(e.target.value)}
+            placeholder="Filter Tasks"
+          />
+        </span>
+        <span className="no">Number of Tasks: {tasks.length} </span>
+      </div>
+      {filterResult.map((task) => {
+        return <TaskListItem key={task.id} filterDisplay={task} />;
       })}
     </div>
   );
--- src/components/TaskListItem.js
+++ src/components/TaskListItem.js
@@ -2,18 +2,20 @@ import React from "react";
 import { useDispatch } from "react-redux";
 import { deleteTask } from "../actions/allActions";
 
-const TaskListItem = ({ task }) => {
+const TaskListItem = ({ filterDisplay }) => {
   const dispatch = useDispatch();
 
   return (
     <div>
       <div className="task-item container">
-        <span>{task.id.length > 1 ? task.id[0] : task.id}</span>
-        <h5>{task.taskName}</h5>
+        <span>
+          {filterDisplay.id.length > 1 ? filterDisplay.id[0] : filterDisplay.id}
+        </span>
+        <h5>{filterDisplay.taskName}</h5>
         <button className="btn btn-primary">Edit</button>
         <button
           className="btn btn-danger"
-          onClick={() => dispatch(deleteTask(task.id))}
+          onClick={() => dispatch(deleteTask(filterDisplay.id))}
         >
           Delete
         </button>
