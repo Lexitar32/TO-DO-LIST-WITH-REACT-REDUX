08035ea62db32f9d1e24ea2cb681c58abfd347dd
Arimoroolamilekan <arimoroolamilekan1@gmail.com>
2021-05-25 18:02:23 +0100

    committed

--- src/store.js
+++ src/store.js
@@ -7,9 +7,9 @@ const middleware = [thunk];
 const store = createStore(
   rootReducer,
   compose(
-    applyMiddleware(...middleware)
+    applyMiddleware(...middleware),
 
-    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
 
